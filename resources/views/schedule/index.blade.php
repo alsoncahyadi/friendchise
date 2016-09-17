d420e4d4eb6e5dd1d9669ab8c271c9d135ff6c81
zhorifiandi <arizho16@gmail.com>
2016-09-18 01:53:16 +0700

    nih son
    guatambahin

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -129,7 +129,7 @@
                 <div class="col-lg-5 col-md-5"></div>
                 <div class="col-lg-4 col-md-4">
                     <a href="franchisee" class="contact_btn">
-                        Dashboard Mitra
+                        Dashboard Pusat
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-3"></div>
