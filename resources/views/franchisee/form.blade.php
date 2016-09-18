6c7e321d211a061cba98f53fc343a2434b4adff5
zhorifiandi <arizho16@gmail.com>
2016-09-18 09:54:23 +0700

    button edit franchisee

--- resources/views/franchisee/form.blade.php
+++ resources/views/franchisee/form.blade.php
@@ -1,33 +1,33 @@
-            <div class="form-group">
+            <div class="form">
                 {!! Form::label('nama', 'Nama', ['class' => 'input' , 'style'=>'font-size : 18px']) !!}
                 {!! Form::text('nama', null, ['class' => 'input']) !!}
             </div>
 
-            <div class="form-group" >
-                {!! Form::label('alamat', 'Alamat', ['class' => 'control-label form' , 'style'=>'font-size : 18px']) !!}
+            <div class="form" >
+                {!! Form::label('alamat', 'Alamat', ['class' => 'form' , 'style'=>'font-size : 18px']) !!}
                 {!! Form::text('alamat', null, ['class' => 'form-control']) !!}
             </div>
 
-            <div class="form-group" >
-                {!! Form::label('kecamatan', 'Kecamatan', ['class' => 'control-label form' , 'style'=>'font-size : 18px']) !!}
+            <div class="form" >
+                {!! Form::label('kecamatan', 'Kecamatan', ['class' => 'form' , 'style'=>'font-size : 18px']) !!}
                 {!! Form::text('kecamatan', null, ['class' => 'form-control']) !!}
             </div>
 
-            <div class="form-group" >
-                {!! Form::label('kota', 'Kota', ['class' => 'control-label form' , 'style'=>'font-size : 18px']) !!}
+            <div class="form" >
+                {!! Form::label('kota', 'Kota', ['class' => 'form' , 'style'=>'font-size : 18px']) !!}
                 {!! Form::text('kota', null, ['class' => 'form-control']) !!}
             </div>
 
-            <div class="form-group" >
-                {!! Form::label('username', 'Username', ['class' => 'control-label form' , 'style'=>'font-size : 18px']) !!}
+            <div class="form" >
+                {!! Form::label('username', 'Username', ['class' => 'form' , 'style'=>'font-size : 18px']) !!}
                 {!! Form::text('username', null, ['class' => 'form-control']) !!}
             </div>
 
-            <div class="form-group" >
-                {!! Form::label('password', 'Password', array('class' => 'control-label form', 'type' => 'password' , 'style'=>'font-size : 18px')) !!}
+            <div class="form" >
+                {!! Form::label('password', 'Password', array('class' => ' form', 'type' => 'password' , 'style'=>'font-size : 18px')) !!}
                 {!! Form::text('password', null, ['class' => 'form-control']) !!}
             </div>
 
-            <div class ="form-group">
-                {!! Form::submit($buttonText, ['class' => 'contact_btn form-control form' , 'style'=>'font-size : 14px; padding : 14px 14px 14px 14px']) !!}
+            <div class ="form">
+                {!! Form::submit($buttonText, ['class' => 'contact_btn  form' , 'style'=>'font-size : 14px; padding : 14px 14px 14px 14px']) !!}
             </div>
